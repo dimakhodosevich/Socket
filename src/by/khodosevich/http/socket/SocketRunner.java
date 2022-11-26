548acd06ca91bef60c97d9a3110b07183480ef22
dimakhodosevich95@gmail.com <102683640+dimakhodosevich@users.noreply.github.com>
2022-11-26 22:33:54 +0300

    Make simple request to google.com. Use try with resourses - socket implements autocloseable interface. Recieve byte[] length=179.

--- src/by/khodosevich/http/socket/SocketRunner.java
+++ src/by/khodosevich/http/socket/SocketRunner.java
@@ -1,5 +1,36 @@
 package by.khodosevich.http.socket;
 
-public class SocketRunner {
+import java.io.*;
+import java.net.Socket;
 
+public class SocketRunner {
+    public static void main(String[] args) {
+        // http - 80
+        // https - 443
+        // tcp port
+
+        try {
+            Socket socket = new Socket("google.com", 80);
+
+            OutputStream outputStream = socket.getOutputStream();
+            InputStream inputStream = socket.getInputStream();
+
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+            dataOutputStream.writeUTF("Hello world");
+
+            byte[] response = dataInputStream.readAllBytes();
+            System.out.println(response.length);
+
+            socket.close();
+            outputStream.close();
+            inputStream.close();
+            dataOutputStream.close();
+            dataInputStream.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
