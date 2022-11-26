3df219852ab6423da1277240273c8aae3051b0d9
dimakhodosevich95@gmail.com <102683640+dimakhodosevich@users.noreply.github.com>
2022-11-26 22:45:02 +0300

    Create InetAddress object - helps us to work with any server by name(DNS).

--- src/by/khodosevich/http/socket/SocketRunner.java
+++ src/by/khodosevich/http/socket/SocketRunner.java
@@ -1,7 +1,10 @@
 package by.khodosevich.http.socket;
 
 import java.io.*;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class SocketRunner {
     public static void main(String[] args) {
@@ -9,28 +12,31 @@ public class SocketRunner {
         // https - 443
         // tcp port
 
+//        Inet4Address (Inet6Address) - works with our address, can send to our Socket as first argument
+        InetAddress inetAddress = null;
+
         try {
-            Socket socket = new Socket("google.com", 80);
+            inetAddress = Inet4Address.getByName("google.com");
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
 
-            OutputStream outputStream = socket.getOutputStream();
-            InputStream inputStream = socket.getInputStream();
 
-            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-            DataInputStream dataInputStream = new DataInputStream(inputStream);
+        try (Socket socket = new Socket(inetAddress, 80);
+             OutputStream outputStream = socket.getOutputStream();
+             InputStream inputStream = socket.getInputStream();
+             DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+             DataInputStream dataInputStream = new DataInputStream(inputStream);
+        ) {
 
             dataOutputStream.writeUTF("Hello world");
-
             byte[] response = dataInputStream.readAllBytes();
             System.out.println(response.length);
-
-            socket.close();
-            outputStream.close();
-            inputStream.close();
-            dataOutputStream.close();
-            dataInputStream.close();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+
     }
 }
